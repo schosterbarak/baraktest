63039ddfad6493747cdbf4d76d7140903eb97920
Barak Schoster Goihman <schosterbarak@gmail.com>
2020-05-21 10:27:27 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
-  # bucket does not have access logs
+  # bucket does not have access logs 
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
